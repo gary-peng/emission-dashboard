3690a120c42095a1244f146d83cb068264df6145
Gary Peng <gary-peng@users.noreply.github.com>
2020-10-06 20:20:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# emission-dashboard
+ A web dashboard to for Smart 3D Printer Emission Monitor
---
+++ fetch-data.js
@@ -0,0 +1,33 @@
+var deviceID = "400023000c47363330353437";
+var accessToken = "ce8173708830fa0c4e19dc9c484d6d8e5d50a468";
+
+var pullVoc;
+var pullRisk;
+var pullAction;
+
+function getData() {
+    pullVoc = setInterval(getVoc, 1000);
+    pullRisk = setInterval(getRisk, 1000);
+    pullAction = setInterval(getAction, 1000);
+}
+
+function getVoc() {
+    var requestURL = "https://api.particle.io/v1/devices/" +deviceID + "/voc/?access_token=" + accessToken;
+    $.getJSON(requestURL, function(json) {
+        $("#voc").text(json.result);
+    });
+}
+
+function getRisk() {
+    var requestURL = "https://api.particle.io/v1/devices/" +deviceID + "/risk/?access_token=" + accessToken;
+    $.getJSON(requestURL, function(json) {
+        $("#risk").text(json.result);
+    });
+}
+
+function getAction() {
+    var requestURL = "https://api.particle.io/v1/devices/" +deviceID + "/action/?access_token=" + accessToken;
+    $.getJSON(requestURL, function(json) {
+        $("#action").text(json.result);
+    });
+}
