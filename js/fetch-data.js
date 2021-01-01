83b3cdd9f831ea7e6dffc86c6cfaebb6dc750aec
Gary Peng <gary-peng@users.noreply.github.com>
2021-01-01 13:58:00 -0800

    Formatting

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,25 +1,21 @@
-var deviceID = "400023000c47363330353437";
-var accessToken = "ce8173708830fa0c4e19dc9c484d6d8e5d50a468";
-
-var pullVoc;
-var pullRisk;
-var pullAction;
+const deviceID = '400023000c47363330353437';
+const accessToken = 'ce8173708830fa0c4e19dc9c484d6d8e5d50a468';
+const requestURL = 'https://api.particle.io/v1/devices/' +deviceID + '/voc/?access_token=' + accessToken;
 
 var bar;
 
 function getData() {
-    pullVoc = setInterval(getVoc, 1000);
-    pullRisk = setInterval(getRisk, 1000);
-    pullAction = setInterval(getAction, 1000);
+    setInterval(getVoc, 1000);
+    setInterval(getRisk, 1000);
+    setInterval(getAction, 1000);
 
-    bar = new ldBar(".myBar", {
-        "preset": "fan"
+    bar = new ldBar('.myBar', {
+        'preset': 'fan'
     });
 }
 
 function getVoc() {
-    var requestURL = "https://api.particle.io/v1/devices/" +deviceID + "/voc/?access_token=" + accessToken;
-    $.getJSON(requestURL, function(json) {
+    $.getJSON(requestURL, (json) => {
         bar.set(
             json.result,
             false
@@ -28,15 +24,13 @@ function getVoc() {
 }
 
 function getRisk() {
-    var requestURL = "https://api.particle.io/v1/devices/" +deviceID + "/risk/?access_token=" + accessToken;
-    $.getJSON(requestURL, function(json) {
-        $("#risk").text(json.result);
+    $.getJSON(requestURL, (json) => {
+        $('#risk').text(json.result);
     });
 }
 
 function getAction() {
-    var requestURL = "https://api.particle.io/v1/devices/" +deviceID + "/action/?access_token=" + accessToken;
-    $.getJSON(requestURL, function(json) {
-        $("#action").text(json.result);
+    $.getJSON(requestURL, (json) => {
+        $('#action').text(json.result);
     });
 }
